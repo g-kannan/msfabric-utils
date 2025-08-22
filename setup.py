0164f8f7b25c5d9a9b764876c5e1a30ea6c5f26b
Kannan <g-kannan@outlook.com>
2025-08-23 00:29:54 +0530

    version for header

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shared-utils',
-    version='0.1.12',
+    version='0.1.13',
     description='Shared utility functions for MS Fabric',
     packages=find_packages(),
     install_requires=[
